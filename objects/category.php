d185b75d91d1bd80c0f48eef16825ba97d9e2bbe
Kursat ASLAN <32969134+kursataslan@users.noreply.github.com>
2021-12-09 11:17:18 +0300

    Update category.php

--- objects/category.php
+++ objects/category.php
@@ -1,11 +1,11 @@
 <?php
 class Category{
   
-    // database connection and table name
+
     private $conn;
     private $table_name = "categories";
   
-    // object properties
+
     public $id;
     public $name;
     public $description;
@@ -14,10 +14,9 @@ class Category{
     public function __construct($db){
         $this->conn = $db;
     }
-  
-    // used by select drop-down list
+
     public function readAll(){
-        //select all data
+ 
         $query = "SELECT
                     id, name, description
                 FROM
@@ -31,10 +30,10 @@ class Category{
         return $stmt;
     }
 
-// used by select drop-down list
+
 public function read(){
   
-    //select all data
+
     $query = "SELECT
                 id, name, description
             FROM
